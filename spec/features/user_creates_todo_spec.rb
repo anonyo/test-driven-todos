e06500d1601122aa8414bfbdb80b5fae285f195a
Anonyo Hossain <anonyo.hossain@gmail.com>
2015-08-30 10:34:22 -0400

    Add sign_in method to spec support

--- spec/features/user_creates_todo_spec.rb
+++ spec/features/user_creates_todo_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 feature 'User creates todo' do
   scenario 'successfully' do
-    visit root_path
+    sign_in
     click_on 'Add New Todo'
     fill_in 'Title', with: 'Need to pick up lunch.'
     click_on 'Submit'
