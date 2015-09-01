3c6a6a8078dd8b2f9579b596d035803094b43a69
Anonyo Hossain <anonyo.hossain@gmail.com>
2015-09-01 08:43:42 -0400

    Refactor todo_spec to have more readable expectations

--- spec/models/todos_spec.rb
+++ spec/models/todos_spec.rb
@@ -4,13 +4,13 @@ describe Todo do
   describe '#completed?' do
     it 'returns true if completed_at is set' do
       todo = Todo.new(completed_at: Time.current)
-      expect(todo.completed?).to be true
+      expect(todo).to be_completed
     end
 
     it 'returns false if completed_at is nil' do
       todo = Todo.new(completed_at: nil)
 
-      expect(todo.completed?).to be false
+      expect(todo).not_to be_completed
     end
   end
 
