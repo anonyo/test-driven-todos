b262863bfd931834a4389f67ebb935824b7a0390
Anonyo Hossain <anonyo.hossain@gmail.com>
2015-09-02 09:42:02 -0400

    Rename completion controller

--- app/controllers/completions_controller.rb
+++ app/controllers/completions_controller.rb
@@ -1,6 +1,11 @@
-class CompletionController < ApplicationController
+class CompletionsController < ApplicationController
   def create
     todo.complete!
+    redirect_to root_path
+  end
+
+  def destroy
+    todo.incomplete!
     redirect_to root_path
   end
   private
